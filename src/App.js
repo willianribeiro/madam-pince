4cdbec5239a817d2472ed99cdab5de622f5f370b
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-11-15 18:55:52 -0200

    Example of user of the MadamPinceApi

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import MadamPinceApi from './services/MadamPinceApi'
 
 class App extends Component {
   render() {
+
+    const promise2 = MadamPinceApi.get_library()
+    promise2.then(response => console.log('get_library()', response))
+
+    const promise3 = MadamPinceApi.list_entries()
+    promise3.then(response => console.log('list_entries()', response))
+
+    const promise4 = MadamPinceApi.get_entry('Jk5Jb3hCV3g3dXhIM3V1Z3RmQGU')
+    promise4.then(response => console.log('get_entry(id)', response))
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hello
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

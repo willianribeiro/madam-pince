e8e1e9b5e3f3d268999cfc2143b6debb22d4d1aa
Willian Ribeiro <willianribeiro.contato@gmail.com>
2018-05-02 18:07:35 -0300

    Change book details modal design (WIP)

--- src/components/BookDetailsModal.js
+++ src/components/BookDetailsModal.js
@@ -2,22 +2,48 @@ import React from 'react'
 import styled from 'styled-components'
 
 const BookDetailsModal = ({ fields, onClose }) => {
-  const pauseEvent = e => {
-    e.stopPropagation()
-    e.preventDefault()
+  // const pauseEvent = e => {
+  //   e.stopPropagation()
+  //   e.preventDefault()
+  // }
+
+  const getBookCoverUrl = () => {
+    const covers = fields.filter(field => {
+      return (
+        field.name === "Capa" &&
+        field.type === "image"
+      )
+    })
+
+    return covers.length > 0
+      ? covers[0].value
+      : "https://place-hold.it/150x200"
+  }
+
+  const getFieldsToList = () => {
+    return fields.filter(field => {
+      return (field.id !== 30 && field.id !== 31 && field.id !== 8)
+    })
   }
 
   return (
-    <Modal onClick={onClose} >
-      <ModalBox onClick={pauseEvent}>
+    <Modal>
+      <ModalOverlay onClick={onClose} />
+      <ModalBox>
         <ModalClose onClick={onClose}>&#10060;</ModalClose>
 
-        {fields.map(field => (
-          <ModalField key={field.id}>
-            <strong>{field.name}:</strong>
-            <ModalLabel>{field.value}</ModalLabel>
-          </ModalField>
-        ))}
+        <BookCover>
+          <BookCoverImg src={getBookCoverUrl()} />
+        </BookCover>
+
+        <BookFields>
+          {getFieldsToList().map(field => (
+            <BookField key={field.id}>
+              <BookFieldTitle>{field.name}:</BookFieldTitle>
+              <BookFieldValue>{field.value || '-- não disponível --'}</BookFieldValue>
+            </BookField>
+          ))}
+        </BookFields>
       </ModalBox>
     </Modal>
   )
@@ -32,6 +58,15 @@ const Modal = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
+  overflow-y: auto;
+`
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   background-color: rgba(0, 0, 0, .9);
 `
 
@@ -39,6 +74,7 @@ const ModalBox = styled.div`
   position: relative;
   display: inline-block;
   padding: 36px;
+  margin: 8px;
   width: 100%;
   max-width: 800px;
   border: 1px solid #ccc;
@@ -60,12 +96,28 @@ const ModalClose = styled.div`
   cursor: pointer;
 `
 
-const ModalField = styled.div`
-  margin-bottom: 8px;
+const BookCover = styled.div`
+  margin-bottom: 16px;
 `
 
-const ModalLabel = styled.label`
-  margin-left: 8px;
+const BookCoverImg = styled.img`
+
+`
+
+const BookFields = styled.div`
+
+`
+
+const BookField = styled.div`
+  margin-bottom: 16px;
+`
+
+const BookFieldTitle = styled.strong`
+  display: block;
+  margin-bottom: 4px;
+`
+
+const BookFieldValue = styled.p`
   word-wrap: break-word;
 `
 

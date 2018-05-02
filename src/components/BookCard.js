279f300adcd7e6249204cc7d9427c668f377771b
Willian Ribeiro <willianribeiro.contato@gmail.com>
2018-05-02 16:49:56 -0300

    Improve book card's design

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 const BookCard = ({
   bookId,
@@ -9,13 +10,40 @@ const BookCard = ({
   onClick
 }) => {
   return (
-    <article onClick={() => onClick(bookId)} key={bookId} style={{ cursor: 'pointer' }}>
-      <h2>{bookTitle}</h2>
-      <p>{bookSubtitle}</p>
-      <p>{bookAuthor}</p>
-    </article>
+    <Card onClick={() => onClick(bookId)} key={bookId}>
+      <CardTitle>{bookTitle}</CardTitle>
+      {bookSubtitle && <CardSubtitle>{bookSubtitle}</CardSubtitle>}
+      <CardAuthor>{bookAuthor}</CardAuthor>
+    </Card>
   )
 }
+
+const Card = styled.article`
+  padding: 16px;
+  cursor: pointer;
+
+  & + & {
+    border-top: 1px solid #ccc;
+  }
+
+  &:hover {
+    background-color: #f7f7f7;
+  }
+`
+
+const CardTitle = styled.h2`
+  font-size: 1.5rem
+`
+
+const CardSubtitle = styled.p`
+  margin-top: .3rem;
+  font-size: 1.3rem;
+  `
+
+const CardAuthor = styled.p`
+  margin-top: 1rem;
+  font-size: 1rem;
+`
 
 BookCard.propTypes = {
   bookId: PropTypes.string.isRequired,
